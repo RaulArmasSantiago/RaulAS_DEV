87206b4bb273f5244ceff895761e1451736613b6
Raul Armas Santiago <raularmassantiago@gmail.com>
2026-03-08 22:47:12 -0600

    Adapter vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "hybrid",
+  adapter: vercel(),
   integrations: [tailwind()],
 });
